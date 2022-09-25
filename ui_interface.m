5da0aa6a185334ba70db1ec9ebd1375af4f5abf4
josealbasit <83695122+josealbasit@users.noreply.github.com>
2022-09-25 22:26:58 +0200

    Add files via upload

--- ui_interface.m
+++ ui_interface.m
@@ -4,12 +4,13 @@ selectedModelstr=num2str(selectedModel);
 close all
 clear h
 
-graphics_toolkit qt
+%graphics_toolkit qt
 
 h.ax = axes ("position", [0.05 0.42 0.5 0.5]);
 h.fcn = @(p) f([initParam]);
 %set (get (h.ax, "title"), "string", equationName);
-
+r2=0;
+errorOptim=0;
 
 
 
@@ -24,12 +25,8 @@ function update_plot (obj, init = false)
     case {h.print_pushbutton}
       fn =  uiputfile ("*.png");
       print (fn);
-    case {h.grid_checkbox}
-      v = get (gcbo, "value");
-      grid (merge (v, "on", "off"));
-    case {h.minor_grid_toggle}
-      v = get (gcbo, "value");
-      grid ("minor", merge (v, "on", "off"));
+    case{h.save_pushbotton}
+      
     case {h.p1_slider}
       recalc = true;
     case {h.p2_slider}
@@ -43,25 +40,29 @@ function update_plot (obj, init = false)
     p(1) = get (h.p1_slider, "value");
     p(2) = get (h.p2_slider, "value");
     p(3) = get (h.p3_slider, "value");
-
-    set (h.p1_label, "string", sprintf ("Parameter 1", p(1)));
-    set (h.p2_label, "string", sprintf ("Parameter 2", p(2)));
-    set (h.p3_label, "string", sprintf ("Parameter 3", p(3)));
-
+    label_1=num2str(p(1));
+    label_2=num2str(p(2));
+    label_3=num2str(p(3));
+    set (h.p1_label, "string",  ["Parameter 1 = " label_1] );
+    set (h.p2_label, "string", ["Parameter 2 = " label_2]);
+    set (h.p3_label, "string", ["Parameter 3 = " label_3]);
     initParam=p
     end
     [optimParam r2 errorOptim]=optimizationProcess(x,y0,f_min,f,initParam,1);
+    set(h.statistics_r2,"string",["R2=" num2str(r2)]);
+    set(h.statistics_error,"string",["Error optim= " num2str(errorOptim)]);
     y_optim_plot=f_plot(optimParam);
     if (init)
       h.plot = plot (x , y0,'+');
       hold on
       h.plot = plot(x_plot,y_optim_plot,'r')
       guidata (obj, h);
+      r2=0.2;
+      errorOptim=1e-2;
     elseif(recalc)
       h.plot = plot (x , y0,'+');
       hold on
       h.plot=plot(x_plot, y_optim_plot,'b');
-      %set (h.plot, "ydata", y);
     endif
   endif
 
@@ -91,43 +92,27 @@ if (selectedModel==1)
 
 endif
 
-h.statistics_text = uicontrol ("style", "text",
-                                "units", "normalized",
-                                "string", equationName,
-                                "horizontalalignment", "left",
-                                "position", [0.6 0.85 0.35 0.08]);
+##Label 
 
-h.plot_title_label = uicontrol ("style", "text",
-                                "units", "normalized",
-                                "string", equationName,
-                                "horizontalalignment", "left",
-                                "position", [0.6 0.85 0.35 0.08]);
 
-h.plot_title_edit = uicontrol ("style", "edit",
-                               "units", "normalized",
-                               "string", ["Equation " selectedModelstr],
-                               "callback", @update_plot,
-                               "position", [0.6 0.80 0.35 0.06]);
-
-## grid
-h.grid_checkbox = uicontrol ("style", "checkbox",
-                             "units", "normalized",
-                             "string", "show grid\n(checkbox)",
-                             "value", 0,
-                             "callback", @update_plot,
-                             "position", [0.6 0.65 0.35 0.09]);
-
-h.minor_grid_toggle = uicontrol ("style", "togglebutton",
-                                 "units", "normalized",
-                                 "string", "minor\n(togglebutton)",
-                                 "callback", @update_plot,
-                                 "value", 0,
-                                 "position", [0.77 0.65 0.18 0.09]);
+h.label = uicontrol ("style", "text",
+                           "units", "normalized",
+                           "string", equationName,
+                           "fontweight","bold",
+                           "horizontalalignment", "left",
+                           "position", [0.25 0.93 0.2 0.05]);
 
 ## print figure
 h.print_pushbutton = uicontrol ("style", "pushbutton",
                                 "units", "normalized",
-                                "string", "print plot\n(pushbutton)",
+                                "string", "Save plot",
+                                "callback", @update_plot,
+                                "position", [0.6 0.45 0.35 0.09]);
+                                
+# Save parameters
+                                h.save_pushbotton = uicontrol ("style", "pushbutton",
+                                "units", "normalized",
+                                "string", "Save parameters' statistics",
                                 "callback", @update_plot,
                                 "position", [0.6 0.45 0.35 0.09]);
 ## p1
@@ -161,7 +146,7 @@ h.p2_slider = uicontrol ("style", "slider",
                             "SliderStep",[0.01 0.01],
                             "units", "normalized",
                             "string", "slider",
-                            "callback", @update_plot,
+                            "callback",s,
                             "value", initParam(2),
                             "position", [0.05 0.1 0.35 0.06]);
 % p3
@@ -182,23 +167,24 @@ h.p3_slider = uicontrol ("style", "slider",
                             "value", initParam(3),
                             "position", [0.45 0.25 0.35 0.06]);
 
-% p4
-
-h.p4_label = uicontrol ("style", "text",
+h.statistics = uicontrol ("style", "text",
                            "units", "normalized",
-                           "string", "Parameter 4",
+                           "string", "Statistics",
+                           "fontweight","bold",
                            "horizontalalignment", "left",
-                           "position", [0.455 0.15 0.35 0.08]);
-
-h.p4_slider = uicontrol ("style", "slider",
-                            "Min" , -100,
-                            "Max",100,
-                            "SliderStep",[0.01 0.01],
+                           "position", [0.6 0.85 0.2 0.1]);
+h.statistics_r2 = uicontrol ("style", "text",
                            "units", "normalized",
-                            "string", "slider",
-                            "callback", @update_plot,
-                            "value", initParam(2),
-                            "position", [0.45 0.1 0.35 0.06]);
+                           "string", ["R2= " num2str(r2) ],
+                           "fontweight","bold",
+                           "horizontalalignment", "left",
+                           "position", [0.6 0.75 0.2 0.05]);
+h.statistics_error = uicontrol ("style", "text",
+                           "units", "normalized",
+                           "string", ["Relative error= " num2str(errorOptim) ],
+                           "fontweight","bold",
+                           "horizontalalignment", "left",
+                           "position", [0.6 0.7 0.2 0.05]);
 
 
 ## linecolor
